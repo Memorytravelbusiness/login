ed65f3fa975e90a2abce3556f36ec1cfde72218f
Memorytravelbusiness <113474997+Memorytravelbusiness@users.noreply.github.com>
2022-09-17 09:04:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function onSuccess(googleUser) {
+	console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+	console.log(error);
+}
+function renderButton() {
+	 gapi.signin2.render('login-google', {
+		'scope': 'profile email',
+		'width': 240,
+		'height': 50,
+		'longtitle': true,
+		'theme': 'dark',
+		'onsuccess': onSuccess,
+		'onfailure': onFailure
+	 });
+}
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
